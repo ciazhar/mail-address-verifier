62e881ee5f37206a7ef0e7408a83bff6cdccff24
ciazhar <ciazhar.id@gmail.com>
2018-02-21 15:29:27 +0700

    update unit test

--- src/test/kotlin/co/mailtarget/mailaddressverifier/MailAddressVerifierTest.kt
+++ src/test/kotlin/co/mailtarget/mailaddressverifier/MailAddressVerifierTest.kt
@@ -1,5 +1,14 @@
 package co.mailtarget.mailaddressverifier
 
+import org.junit.Test
+
 class MailAddressVerifierTest{
+
+    @Test
+    fun validateEmailAddress(){
+        val str = "rully.arifin@ibsolutions.co.id"
+        val result = MailAddressVerifier.validate(str)
+        print(result)
+    }
 
 }
