d03c3935a524ff844d9337d7f183e93dba79d31e
ciazhar <ciazhar.id@gmail.com>
2018-02-22 10:49:10 +0700

    add comment

--- src/main/kotlin/co/mailtarget/mailaddressverifier/resource/AddressStatus.kt
+++ src/main/kotlin/co/mailtarget/mailaddressverifier/resource/AddressStatus.kt
@@ -6,11 +6,17 @@ package co.mailtarget.mailaddressverifier.resource
  */
 
 enum class AddressStatus {
+    /// absolutely valid
     valid,
+    /// wrong email regex
     wrongSchema,
+    /// mail server not exists
     noMxRecord,
+    /// user not exists in mail server
     notRegistered,
+    /// mail server exists and mail address may exits, but not include in gmail/yahoo/outlook
     notExistsInGmailOrYahooOrOutlook,
+    /// mail address unknown
     unknown;
 
     private var mail: String? = null
